1a5f19da74735ed4f4294b616973bc827aea2a3a
shinsehoon <iszzon@gmail.com>
2022-04-05 00:09:38 +0900

    리스트 중에서 제일 많이 나온수 그 중에서도 제일 작은 수 알아내는 문제

---
+++ src/MigratoryBirds.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MigratoryBirds {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(9,9));
+
+        List<Integer> result = new ArrayList<>(Arrays.asList(0,0,0,0,0,0,0,0,0,0));
+
+        for(Integer num : arr){
+            result.set(num, result.get(num)+1);
+        }
+
+        int max = Collections.max(result);
+        System.out.println("제일 많이 나온 횟수 : " + max);
+        System.out.println("max : "+max+ "\t 그 값의 인덱스 : " + result.indexOf(max));
+    }
+}
