35dd0d95da3e415daa013b1a8d808b8101090f0d
shshin <shshin@geotwo.com>
2022-04-05 18:56:37 +0900

    그래고리력 율리우스력 기준으로 날짜 계산하기 기본 세팅

---
+++ src/DayOfTheProgrammer.java
@@ -0,0 +1,21 @@
+public class DayOfTheProgrammer {
+    public static void main(String[] args) {
+        // 1700 ~ 2700
+        // 1917까지 율리우스력 그 이후 그레고리력
+        int year = 2016;
+
+        if(year <= 2017){
+            // 율리우스 력
+        }else{
+            // 그래고리력
+            //        if(year % 400 == 0 && (year % 4 == 0 && year % 100 != 0)){
+            //            System.out.println("윤달"); // 366
+            //        }else{
+            //            System.out.println("평달"); // 365
+            //        }
+        }
+
+
+
+    }
+}
