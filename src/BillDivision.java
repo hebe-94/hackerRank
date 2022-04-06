21397cca786d99807b4e951feeb04478cb0a8636
shshin <shshin@geotwo.com>
2022-04-06 18:19:53 +0900

    먹고 내야하는돈과 냈던돈이 일치한지 확인하는 문제 기본 세팅

---
+++ src/BillDivision.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BillDivision {
+    public static void main(String[] args) {
+        ArrayList<Integer> scores = new ArrayList<>(Arrays.asList(3,4,21,36,10,28,35,5,24,42));
+        // 먹지 않은 인덱스
+        int k;
+        // 받은돈
+        int b;
+        // 결국 받은돈과 내야하는 돈의 차이를 비교해서
+        // 제대로 줬는지 아닌지 판단
+    }
+}
