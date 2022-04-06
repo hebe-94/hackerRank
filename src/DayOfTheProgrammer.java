6e58228dbb40f763179a756ad93a99d84a6fc181
shinsehoon <iszzon@gmail.com>
2022-04-06 22:01:40 +0900

    1. 달이 변경된 1918을 제외 2. 율리우스력 3. 그래고리력 4. 나머지 평달

--- src/DayOfTheProgrammer.java
+++ src/DayOfTheProgrammer.java
@@ -2,20 +2,26 @@ public class DayOfTheProgrammer {
     public static void main(String[] args) {
         // 1700 ~ 2700
         // 1917까지 율리우스력 그 이후 그레고리력
-        int year = 2016;
+        int year = 1915;
 
-        if(year <= 2017){
-            // 율리우스 력
+        String result;
+        // 1918년에는 1월 31일 다음이 2월 14일  (d+14)
+        // 윤년이 아니므로 9월 12일 +14일  9월 26일
+        if(year == 1918){
+            result = "26.09.1918";
+
+        // 1918년보다 작으면 400으로 나누어 떨어질때만 (율리우스력)
+        }else if(year < 1918 && year % 4 == 0){
+            result = "12.09." + year;
+
+        // 1918년보다 크면 400으로 나누어 떨어지면서 4의배수, 100으로 안떨어지는(그래고리력)
+        }else if(year > 1918 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))){
+            result = "12.09." + year;
         }else{
-            // 그래고리력
-            //        if(year % 400 == 0 && (year % 4 == 0 && year % 100 != 0)){
-            //            System.out.println("윤달"); // 366
-            //        }else{
-            //            System.out.println("평달"); // 365
-            //        }
+            result = "13.09." + year;
         }
 
-
+        System.out.println(result);
 
     }
 }
