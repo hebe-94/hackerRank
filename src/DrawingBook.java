e009127241e5cb9fb3adc41ff85d0ee7d1eb657a
shshin <shshin@geotwo.com>
2022-04-07 11:32:35 +0900

    페이지를 몇번 넘여가 하는지 문제 앞에서 세고, 뒤에서 세고 최소값을 출력하는 문제 반복문을 하지 않고 계산으로 출력!

---
+++ src/DrawingBook.java
@@ -0,0 +1,14 @@
+public class DrawingBook {
+    public static void main(String[] args) {
+        // 길이의 반을 쪼개서
+        // 뒤가 편한지 앞이 편한지 판단
+        int n = 6; // 전체 페이지 수
+        int p = 2; // 요청 페이지 수
+
+        if (p <= n/2){
+            System.out.println("[정순] 횟수 : " + p/2);
+        }else{
+            System.out.println("[역순] 횟수 : " + (n/2 - p/2));
+        }
+    }
+}
