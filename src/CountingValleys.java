d2300546e81c5ccaa0931f07e7afb281c8477af2
shinsehoon <iszzon@gmail.com>
2022-04-11 01:06:44 +0900

    계곡을 들어갔다가 나올때 U과 stepCNT를 기준으로 세기

--- src/CountingValleys.java
+++ src/CountingValleys.java
@@ -1,2 +1,21 @@
 public class CountingValleys {
+    public static void main(String[] args) {
+
+        String path = "DDUUDDUDUUUD";
+        int steps = path.length();
+        int stepCNT = 0;
+        int enterCNT = 0;
+        for(int i =0; i<steps; i++){
+            if( path.charAt(i) == 'U'){
+                stepCNT ++;
+                if(stepCNT == 0){
+                    enterCNT++;
+                }
+            }else{
+                stepCNT--;
+            }
+        }
+
+        System.out.println(enterCNT);
+    }
 }
