e8825c3498c8073ff9d37039bf769d31b9247666
shshin <shshin@geotwo.com>
2022-04-11 18:10:48 +0900

    어떤 고양이가 쥐를 먼저 잡을까? 동시 도착하면 고양이끼리 싸운다

---
+++ src/CatAndAMouse.java
@@ -0,0 +1,7 @@
+public class CatAndAMouse {
+    public static void main(String[] args) {
+        // 고양이 2마리와 쥐의 거리를 확인하고 가까운 애가 잡는것
+        // 둘의 거리가 같다면 고양이 끼리 싸운다.
+        // 쥐 - 고1,   쥐 - 고2 의 절대값으로 비교해서 진행할것!
+    }
+}
