a16849e169763de02205d3b86d86945ca8a54dfb
shinsehoon <iszzon@gmail.com>
2022-04-25 22:24:40 +0900

    수업시작 전 남아있는 학생수에 따라 수업 할지 말지 판단

--- src/april_22/AngryProfessor.java
+++ src/april_22/AngryProfessor.java
@@ -2,19 +2,27 @@ package april_22;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class AngryProfessor {
     // Utopian Tree 다음문제
     public static void main(String[] args) {
         // 수업 취소 : YES, 수업 진행 : NO
-        int k = 0;
-        ArrayList<Integer> a = new ArrayList<>(Arrays.asList(3,4,21,36,10,28,35,5,24,42));
-        String resultMsg = "NO";
+        int k = 2;
+        ArrayList<Integer> a = new ArrayList<>(Arrays.asList(0,-1,2,1));
+        String resultMsg = "YES";
         int stuCNT = 0;
-        for(Integer student : a){
-            if(student <= 0){
-                stuCNT++;
+        Collections.sort(a);
+        System.out.println(a);
+        for(Integer data : a){
+            if(data > 0) break;
+            stuCNT++;
+            if(stuCNT >= k){
+                resultMsg = "NO";
+                break;
             }
         }
+
+        System.out.println(resultMsg);
     }
 }
